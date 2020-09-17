250bd31b89ebf8610b17a7096949f597f7546290
Uwumukiz <marceluwumukiza12@gmail.com>
2020-09-17 15:37:16 +0200

    Mastering arrays and looping

---
+++ exercis1.js
@@ -0,0 +1,7 @@
+// Console the letters of each element
+let furniture = ['Table', 'Chairs','Couch'];
+for(let i=0; i<furniture.length; i++){
+    for(let j=0; j<furniture[i].length; j++){
+        console.log(furniture[i][j].charAt(0))
+    }
+}
